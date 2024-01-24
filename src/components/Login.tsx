8f6f5d3c591aa95945f10caf9d8663672e069e71
rfbarcelos <rflbarcelos9@gmail.com>
2024-01-24 16:18:43 -0300

    grupo 1 - Requisito 06 - OK

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isFormValid, setFormValid] = useState(false);
+
+  const navigate = useNavigate();
 
   const validateForm = () => {
     const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -26,6 +29,8 @@ function Login() {
     if (isFormValid) {
       const user = { email };
       localStorage.setItem('user', JSON.stringify(user));
+      navigate('/meals', { replace: true });
+
       console.log('Formulário válido. Enviar dados:', { email, password });
     } else {
       console.log('Formulário inválido. Corrija os campos.');
