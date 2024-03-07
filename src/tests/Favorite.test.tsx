b84af7e247ce6a7d5e488747feafd9f6c4a976d4
Guilherme <gulhermegui772001@gmail.com>
2024-03-06 22:58:27 -0300

    recibes

--- src/tests/Favorite.test.tsx
+++ src/tests/Favorite.test.tsx
@@ -9,7 +9,7 @@ const botaoprofilefavoritos = 'profile-favorite-btn';
 
 describe('RecipeIn', () => {
   it('verificar se ao clicar no botao de favoritos ele redireciona para rota certa', async () => {
-    renderWithRouter(<App />, { route: '/meals/52977/in-progress' });
+    renderWithRouter(<App />, { route: '/meals/53060/in-progress' });
     const RecipeFavorito = await screen.findByTestId(botaodafavoritos);
     await userEvent.click(RecipeFavorito);
     const Profile = await screen.findByTestId(botaoprofile);
@@ -31,46 +31,32 @@ describe('RecipeIn', () => {
     await userEvent.click(botao);
     expect(Meals).not.toBeInTheDocument();
   });
-
-  // it('verificar se ao clicar no compartilhar o link e copiado', async () => {
-  //   renderWithRouter(<App />, { route: '/meals/53069/in-progress' });
-  //   const RecipeFavorito = await screen.findByTestId(botaodafavoritos);
-  //   await userEvent.click(RecipeFavorito);
-  //   const Profile = await screen.findByTestId(botaoprofile);
-  //   await userEvent.click(Profile);
-  //   const favoritos = await screen.findByTestId(botaoprofilefavoritos);
-  //   await userEvent.click(favoritos);
-  //   const Recipecompartilhar = await screen.findByTestId('0-horizontal-share-btn');
-  //   await userEvent.click(Recipecompartilhar);
-  //   const pastedText = await navigator.clipboard.readText();
-  //   const expectedLink = 'http://localhost:3000/meals/undefined';
-  //   expect(pastedText).toBe(expectedLink);
-  // });
-  // it('Verificando os filtro do meals nos favoritos', async () => {
-  //   renderWithRouter(<App />, { route: '/meals/52978' });
-  //   const name = await screen.findByText('Kumpir');
-  //   const RecipeFavorito = await screen.findByTestId(botaodafavoritos);
-  //   await userEvent.click(RecipeFavorito);
-  //   const Profile = await screen.findByTestId(botaoprofile);
-  //   await userEvent.click(Profile);
-  //   const favoritos = await screen.findByTestId(botaoprofilefavoritos);
-  //   await userEvent.click(favoritos);
-  //   const Meals = await screen.findByTestId('filter-by-meal-btn');
-  //   await userEvent.click(Meals);
-  //   expect(name).toBeInTheDocument();
-  // });
-  // it.only('Verificando os filtro do drinks nos favoritos', async () => {
-  //   localStorage.clear();
-  //   renderWithRouter(<App />, { route: '/drinks/17222/in-progress' });
-  //   const RecipeFavorito = await screen.findByTestId(botaodafavoritos);
-  //   await userEvent.click(RecipeFavorito);
-  //   expect(RecipeFavorito).toHaveAttribute('src', '/src/images/blackHeartIcon.svg');
-  //   const Profile = await screen.findByTestId(botaoprofile);
-  //   await userEvent.click(Profile);
-  //   const favoritos = await screen.findByTestId(botaoprofilefavoritos);
-  //   await userEvent.click(favoritos);
-  //   expect(await screen.findByRole('heading', {
-  //     name: 'A1',
-  //   })).toBeInTheDocument();
-  // });
+  it('Testa se ao clicar em compartilhar, o link da receita é salvo no clipboard', async () => {
+    renderWithRouter(<App />, { route: '/drinks/15288/in-progress' });
+    const RecipeFavorito = await screen.findByTestId(botaodafavoritos);
+    await userEvent.click(RecipeFavorito);
+    const Profile = await screen.findByTestId(botaoprofile);
+    await userEvent.click(Profile);
+    const favoritos = await screen.findByTestId(botaoprofilefavoritos);
+    await userEvent.click(favoritos);
+    const shareButton = await screen.findByTestId('0-horizontal-share-btn');
+    expect(shareButton).toBeInTheDocument();
+    await userEvent.click(shareButton);
+    const shareMessage = await screen.findByText('Link copied!');
+    expect(shareMessage).toBeInTheDocument();
+  });
+  it('Testa se ao clicar em compartilhar, o link da receita é salvo no clipboard', async () => {
+    renderWithRouter(<App />, { route: '/meals/52977/in-progress' });
+    const RecipeFavorito = await screen.findByTestId(botaodafavoritos);
+    await userEvent.click(RecipeFavorito);
+    const Profile = await screen.findByTestId(botaoprofile);
+    await userEvent.click(Profile);
+    const favoritos = await screen.findByTestId(botaoprofilefavoritos);
+    await userEvent.click(favoritos);
+    const shareButton = await screen.findByTestId('0-horizontal-share-btn');
+    expect(shareButton).toBeInTheDocument();
+    await userEvent.click(shareButton);
+    const shareMessage = await screen.findAllByText('Link copied!');
+    expect(shareMessage).toBeInTheDocument();
+  });
 });
